fb3d8936182f5fb78106f0b1edde7af0c5d6ffb0
namijain87 <namijain1510@gmail.com>
2026-09-06 22:29:26 +0530

    Add Week 4 array problems

---
+++ src/main/java/arrays/assignment_problems/MaxSubarray.java
@@ -0,0 +1,34 @@
+public class MaxSubarray {
+
+    int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (currentSum + nums[i] > nums[i]) {
+                currentSum = currentSum + nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        MaxSubarray obj = new MaxSubarray();
+
+        int result = obj.maxSubArray(nums);
+
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/arrays/assignment_problems/ThreeSum.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+
+public class ThreeSum {
+
+    int[][] threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        // First pass: count unique triplets
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    count++;
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == rightValue) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        // Create result array
+        int[][] result = new int[count][3];
+
+        // Second pass: store unique triplets
+        int index = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result[index][0] = nums[i];
+                    result[index][1] = nums[left];
+                    result[index][2] = nums[right];
+                    index++;
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == rightValue) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        ThreeSum obj = new ThreeSum();
+
+        int[][] result = obj.threeSum(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(
+                "[" + result[i][0] + ", "
+                + result[i][1] + ", "
+                + result[i][2] + "]"
+            );
+        }
+    }
+}
